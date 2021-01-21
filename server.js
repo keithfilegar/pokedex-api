eea9cc19ace706020be7e061e0afe1cb61c309df
keithfilegar <keith.filegar@gmail.com>
2021-01-21 10:31:10 -0800

    GET /types has basic functionality

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
-app.use((req, res) => {
-    res.send('yo world')
-})
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+function handleGetTypes(req, res) {
+    res.json(validTypes)
+}
+
+app.get('/types', handleGetTypes)
 
 const PORT = 8000;
 
