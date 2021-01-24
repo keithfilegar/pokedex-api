84bea908fd73643c6b382f1853e28b0cf59e21b3
keithfilegar <keith.filegar@gmail.com>
2021-01-23 22:22:31 -0800

    clear console.log()'s, add error handling middleware, respect environment port

--- server.js
+++ server.js
@@ -7,19 +7,18 @@ const POKEDEX = require('./pokedex.json');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting));
 app.use(helmet())
 app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization')
     const apiToken = process.env.API_TOKEN
-    console.log('validate bearer token middleware')
 
     if(!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request' })
     }
-    //move to the next middleware
     next()
 })
 
@@ -50,8 +49,16 @@ function handleGetPokemon(req, res) {
 
 app.get('/pokemon', handleGetPokemon)
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-    console.log(`Server is listening at http://localhost:${PORT}`)
+app.use((error, req, res, next) => {
+    let response
+    if(process.env.NODE_ENV === 'production') {
+        response = {error: { message: 'server error'}}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
 })
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {})
